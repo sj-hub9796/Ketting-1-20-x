587a4315f43c19cb7519c7d979a91e1670706b82
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-06-28 20:11:10 +0200

    Add more config options, add `System.exit` as default instead

--- src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
+++ src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
@@ -43,8 +43,9 @@ public class KettingConfig extends AbstractConfig {
     public final BooleanValue OVERWRITE_FORGE_PERMISSIONS = new BooleanValue("forge.overwrite_forge_permissions", false, "--- WARNING - THIS WILL COMPLETELY DISABLE FORGE PERMISSION CHECKS ---  Overwrite Forge permissions with Bukkit permissions, makes it possible to use a permission manager plugin for modded commands. If true, Forge permissions will be set to 'forge.command.MODDEDCOMMAND' where MODDEDCOMMAND is the name of the modded command.");
 
     public final BooleanValue MERGE_WORLD_SYSTEMS = new BooleanValue("ketting.merge_world_systems", false, "If true, this will attempt to merge both the Forge and Bukkit world system into one, making dimensions exist in the world folder, and Bukkit worlds in their own folder.");
-    public final BooleanValue HALT_FORCE = new BooleanValue("ketting.halt.force", false, "If true, Ketting will forcibly halt the server, once it is supposed to regularly shutdown. This is useful, if some plugins or mods keep the server up unintentionally.");
-    public final IntValue HALT_THREADDUMP_SLEEP = new IntValue("ketting.halt.thread_dump.sleep_time", 0, "If true, Ketting will sleep this many seconds before printing the thread-dump.");
+    public final BooleanValue HALT_EXIT = new BooleanValue("ketting.halt.exit", true, "If true, Ketting will call System.exit(255), once it is supposed to regularly shutdown. This is useful, if some plugins or mods keep the server up unintentionally.");
+    public final BooleanValue HALT_HALT = new BooleanValue("ketting.halt.halt", false, "If true, Ketting will call Runtime.getRuntime().halt(), once it is supposed to regularly shutdown. THIS SKIPS SHUTDOWN HOOKS. SOME STUFF MIGHT BREAK!");
+    public final IntValue HALT_THREADDUMP_SLEEP = new IntValue("ketting.halt.thread_dump.sleep_time", 5000, "If true, Ketting will sleep this many seconds before printing the thread-dump.");
     public final BooleanValue HALT_THREADDUMP_ENABLE = new BooleanValue("ketting.halt.thread_dump.enabled", false, "If true, Ketting will print a Stacktrace of all threads once the server is supposed to regularly shutdown. This option is intended to aid debugging for the option 'ketting.force.halt'.");
     //End of config values
 }
