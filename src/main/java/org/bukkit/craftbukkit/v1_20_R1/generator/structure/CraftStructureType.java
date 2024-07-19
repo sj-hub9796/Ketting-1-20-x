6e6517c6a77b86d005a682dfc65b8d86ba48c2c1
JustRed23 <manuel.bormans@gmail.com>
2024-07-20 01:14:58 +0200

    Fix structures crashing the server when converting structure type to bukkit type with an invalid registry key

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/generator/structure/CraftStructureType.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/generator/structure/CraftStructureType.java
@@ -5,6 +5,8 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 import org.bukkit.craftbukkit.v1_20_R1.util.CraftNamespacedKey;
 import org.bukkit.generator.structure.StructureType;
+import org.kettingpowered.ketting.config.KettingConfig;
+import org.kettingpowered.ketting.core.Ketting;
 
 public class CraftStructureType extends StructureType {
 
@@ -13,7 +15,17 @@ public class CraftStructureType extends StructureType {
             return null;
         }
 
-        return Registry.STRUCTURE_TYPE.get(CraftNamespacedKey.fromMinecraft(BuiltInRegistries.STRUCTURE_TYPE.getKey(minecraft)));
+        //Ketting start - fallback to MODDED if the key is not present in the registry
+        var key = BuiltInRegistries.STRUCTURE_TYPE.getKey(minecraft);
+
+        if (key == null) {
+            if (KettingConfig.getInstance().WARN_ON_UNKNOWN_STRUCTURE_TYPE.getValue())
+                Ketting.LOGGER.warn("Structure type with class {} was unknown, likely modded", minecraft.getClass().getName());
+            return StructureType.MODDED;
+        }
+
+        return Registry.STRUCTURE_TYPE.get(CraftNamespacedKey.fromMinecraft(key));
+        //Ketting end
     }
 
     public static net.minecraft.world.level.levelgen.structure.StructureType<?> bukkitToMinecraft(StructureType bukkit) {
--- src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
+++ src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
@@ -39,6 +39,7 @@ public class KettingConfig extends AbstractConfig {
     public final BooleanValue PRINT_INJECTIONS = new BooleanValue("debug.print_injections", false, "Print all values injected into Bukkit to the console.");
     public final BooleanValue WARN_ON_UNKNOWN_ENTITY = new BooleanValue("debug.warn_on_unknown_entity", true, "Print a warning to the console when an entity unknown to Bukkit is spawned.");
     public final BooleanValue WARN_ON_NULL_NBT = new BooleanValue("debug.warn_on_null_nbt", true, "Print a warning to the console when something tries to set a null NBT tag.");
+    public final BooleanValue WARN_ON_UNKNOWN_STRUCTURE_TYPE = new BooleanValue("debug.warn_on_unknown_structure_type", true, "Print a warning to the console when a structure type is unknown to Bukkit.");
 
     public final BooleanValue OVERWRITE_FORGE_PERMISSIONS = new BooleanValue("forge.overwrite_forge_permissions", false, "--- WARNING - THIS WILL COMPLETELY DISABLE FORGE PERMISSION CHECKS ---  Overwrite Forge permissions with Bukkit permissions, makes it possible to use a permission manager plugin for modded commands. If true, Forge permissions will be set to 'forge.command.MODDEDCOMMAND' where MODDEDCOMMAND is the name of the modded command.");
 
