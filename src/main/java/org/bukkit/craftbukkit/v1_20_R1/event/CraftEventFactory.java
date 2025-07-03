35cf2a8a1a27768811b9bb860e04a95bd5159ed2
sjhub <sj.hub9796@gmail.com>
2025-07-03 21:50:46 +0900

    [Paper-Server] 0876, 0973

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/event/CraftEventFactory.java
@@ -512,7 +512,13 @@ public class CraftEventFactory {
         return callPlayerInteractEvent(who, action, position, direction, itemstack, false, hand, null);
     }
 
+    // Paper start - cancelledItem param
     public static PlayerInteractEvent callPlayerInteractEvent(net.minecraft.world.entity.player.Player who, Action action, BlockPos position, Direction direction, ItemStack itemstack, boolean cancelledBlock, InteractionHand hand, Vec3 targetPos) {
+        return callPlayerInteractEvent(who, action, position, direction, itemstack, cancelledBlock, false, hand, targetPos);
+    }
+
+    public static PlayerInteractEvent callPlayerInteractEvent(net.minecraft.world.entity.player.Player who, Action action, BlockPos position, Direction direction, ItemStack itemstack, boolean cancelledBlock, boolean cancelledItem, InteractionHand hand, Vec3 targetPos) {
+        // Paper end - cancelledItem param
         Player player = (who == null) ? null : (Player) who.getBukkitEntity();
         CraftItemStack itemInHand = CraftItemStack.asCraftMirror(itemstack);
 
@@ -547,6 +553,11 @@ public class CraftEventFactory {
         if (cancelledBlock) {
             event.setUseInteractedBlock(Event.Result.DENY);
         }
+        // Paper start
+        if (cancelledItem) {
+            event.setUseItemInHand(Result.DENY);
+        }
+        // Paper end
         craftServer.getPluginManager().callEvent(event);
 
         return event;
